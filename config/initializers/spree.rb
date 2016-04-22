1f68ff78db6bf4ead735fe0d9d88bcf55e0dd737
Anthony D'Addeo <amdaddeo@gmail.com>
2016-04-22 14:58:40 -0500

    Update line_item_attributes

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -1,17 +1,26 @@
 Rails.application.config.to_prepare do
   Spree::Api::ApiHelpers.order_attributes << :customization_total
 
+  Spree::Api::LineItemsController.line_item_options << {
+      customizations_attributes:
+          [
+              :source_id,
+              :source_type,
+              :configuration_id,
+              :configuration_type,
+              :option_id,
+              :option_type
+          ]
+  }
+
   Spree::PermittedAttributes.line_item_attributes << {
-      options:
-          {
-              customizations_attributes: [
-                  :configuration_id,
-                  :source_id,
-                  :source_type,
-                  :configuration_type,
-                  :option_id,
-                  :option_type
-              ]
-          }
+      customizations_attributes: [
+          :configuration_id,
+          :source_id,
+          :source_type,
+          :configuration_type,
+          :option_id,
+          :option_type
+      ]
   }
 end
