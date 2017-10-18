8f81ebfb299f32008aa15c99d4a44b9d07911460
Anthony D'Addeo <amdaddeo@gmail.com>
2017-10-18 09:44:22 -0500

    Add is_non_standard to permitted attrbs

--- lib/spree/permitted_attributes_decorator.rb
+++ lib/spree/permitted_attributes_decorator.rb
@@ -2,6 +2,6 @@ Spree::PermittedAttributes.module_eval do
   mattr_accessor :item_customization_attributes
 
   class_variable_set(:@@item_customization_attributes, [
-    :user_id, :medium, :name, :size, :markup, :render_url, :source_id, :template_id
+    :user_id, :medium, :name, :size, :markup, :render_url, :source_id, :template_id, :is_non_standard
   ])
 end
