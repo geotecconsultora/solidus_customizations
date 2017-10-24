de30818792365ac25baa7d4781f24a7701e8bc96
Anthony D'Addeo <amdaddeo@gmail.com>
2017-10-24 12:13:16 -0500

    Fix item_customization permitted_attributes

--- lib/spree/permitted_attributes_decorator.rb
+++ lib/spree/permitted_attributes_decorator.rb
@@ -2,6 +2,12 @@ Spree::PermittedAttributes.module_eval do
   mattr_accessor :item_customization_attributes
 
   class_variable_set(:@@item_customization_attributes, [
-    :user_id, :medium, :name, :size, :markup, :render_url, :source_id, :template_id, :is_non_standard
+    :configuration_id,
+    :configuration_type,
+    :source_id,
+    :source_type,
+    :article_id,
+    :article_type,
+    :is_non_standard
   ])
 end
