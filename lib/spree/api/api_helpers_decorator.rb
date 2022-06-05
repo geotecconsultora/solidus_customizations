c687ffb83ba110888e54c504fe793f0c047e5ca8
Martin Gerez <edmargerez@gmail.com>
2022-06-05 03:02:54 -0300

    change

--- lib/spree/api/api_helpers_decorator.rb
+++ lib/spree/api/api_helpers_decorator.rb
@@ -1,4 +1,4 @@
-Spree::Api::ApiHelpers.module_eval do
+Spree::Api::Config.module_eval do
   class_variable_set(
       :@@order_attributes,
       class_variable_get(:@@order_attributes).push(:customization_total))
